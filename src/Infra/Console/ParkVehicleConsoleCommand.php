ebc20dfb51b77147ef91168bf4633b4ddca86cc1
Chris SIMON <chris.simon@openedition.org>
2025-04-07 19:31:57 +0200

    WIP

--- src/Infra/Console/ParkVehicleConsoleCommand.php
+++ src/Infra/Console/ParkVehicleConsoleCommand.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Fulll\Infra\Console;
 
 use Fulll\App\Services\ParkVehicleService;
-use Fulll\Domain\Exceptions\VehicleAlreadyRegisteredException;
 use Fulll\Domain\Models\Vehicle;
+use Fulll\Infra\Repositories\SqLiteVehiculeRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -21,6 +21,7 @@ final class ParkVehicleConsoleCommand extends Command
 {
     public function __construct(
         private readonly ParkVehicleService $parkVehicleService,
+        private SqLiteVehiculeRepository $sqLiteVehiculeRepository,
     ) {
         parent::__construct();
     }
@@ -30,21 +31,29 @@ final class ParkVehicleConsoleCommand extends Command
         $this
             ->setDescription('Park a vehicule')
             ->addArgument('fleetId', InputArgument::REQUIRED, 'Fleet ID')
-            ->addArgument('vehiclePlateNumber', InputArgument::REQUIRED, 'Vehicle Plate Number');
+            ->addArgument('vehiclePlateNumber', InputArgument::REQUIRED, 'Vehicle Plate Number')
+            ->addArgument('lat lng', InputArgument::REQUIRED, 'GPS coordinates')
+            ->addArgument('alt', InputArgument::OPTIONAL, 'ALtitude');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $fleetId = (int) $input->getArgument('fleetId');
-        $vehiclePlateNumber = (int) $input->getArgument('vehiclePlateNumber');
-        $vehicle = new Vehicle($vehiclePlateNumber);
+        $vehiclePlateNumber = (string) $input->getArgument('vehiclePlateNumber');
+        $lat_lng = (string) $input->getArgument('lat lng');
+        $alt = (string) $input->getArgument('alt') ?? null;
+        $vehicle_id = $this->sqLiteVehiculeRepository->findIdByPlateNumber(plateNumber : $vehiclePlateNumber);
+        if (null === $vehicle_id) {
+            throw new \Exception('THe vehicle does not exist');
+        }
+        $vehicle = new Vehicle(id : $vehicle_id, plate_number : $vehiclePlateNumber);
 
         try {
-            $this->registerVehicleService->registerVehicle($vehicle, $fleetId);
-            $output->writeln('<info>✅ Vehicle successfully registered to the fleet.</info>');
+            $this->parkVehicleService->parkVehicle(vehicle : $vehicle, fleet_id : $fleetId, gps_coordinates : $lat_lng, alt : $alt);
+            $output->writeln('<info>✅ Vehicle successfully parked.</info>');
 
             return Command::SUCCESS;
-        } catch (VehicleAlreadyRegisteredException $e) {
+        } catch (\Exception $e) {
             $output->writeln('<comment>⚠️  '.$e->getMessage().'</comment>');
 
             return Command::FAILURE;
---
+++ src/Infra/Repositories/SqLiteVehicleRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Fulll\Infra\Repositories;
+
+class SqLiteVehiculeRepository
+{
+    public function findIdByPlateNumber(string $plateNumber): ?int
+    {
+        try {
+            $db = new \SQLite3($_ENV['DB_PATH']);
+
+            $stmt = $this->db->prepare('SELECT id FROM vehicles WHERE plate_number = :plate');
+            $stmt->bindValue(':plate', $plateNumber, SQLITE3_TEXT);
+            $result = $stmt->execute();
+
+            $row = $result->fetchArray(SQLITE3_ASSOC);
+
+            return $row ? (int) $row['id'] : null;
+        } catch (\Exception $e) {
+            echo 'Error: '.$e->getMessage();
+
+            return false;
+        }
+
+        $db->close();
+    }
+}
